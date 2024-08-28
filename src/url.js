dbc4666d9a3200692726a0180c1057db01940cbb
Dharshan8088 <88958857+Dharshan8088@users.noreply.github.com>
2024-08-28 22:12:52 +0530

    Update url.js

--- src/url.js
+++ src/url.js
@@ -1,1 +1,2 @@
-export const BASE_URL='https://workout-tracker-mern-app.vercel.app/';
+//export const BASE_URL='https://workout-tracker-mern-app.vercel.app/';
+export const BASE_URL='https://workout-tracker-mern-app.onrender.com';
